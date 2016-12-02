bfe12d324e76f0b120d6a5037e43fa2e3535f1ba
OscaRasta <oscarlazo11@hotmail.com>
2016-12-02 00:05:41 -0600

    Update fancywebsocket.js

--- js/fancywebsocket.js
+++ js/fancywebsocket.js
@@ -1,73 +1,54 @@
+
+I have a server in digital ocean which is using StartCOM class I primary intermediate CA for ssl. I have set u a websocket server and I want to make a connection to it from a page which is served by https.
+
+When I try to connect to the websocket using just http it works fine. But when I try to use it over https by changing the websocket uri from ws to wss it does not connect.
+
+What am I doing wrong. Connection is made using fancywebsockets.js
+
+fancywebsockets.js
+
 var FancyWebSocket = function(url)
 {
-	var callbacks = {};
-	var ws_url = url;
-	var conn;
-	
-	this.bind = function(event_name, callback)
-	{
-		callbacks[event_name] = callbacks[event_name] || [];
-		callbacks[event_name].push(callback);
-		return this;
-	};
-	
-	this.send = function(event_name, event_data)
-	{
-		this.conn.send( event_data );
-		return this;
-	};
-	
-	this.connect = function() 
-	{
-		if ( typeof(MozWebSocket) == 'function' )
-		this.conn = new MozWebSocket(url);
-		else
-		this.conn = new WebSocket(url);
-		
-		this.conn.onmessage = function(evt)
-		{
-			dispatch('message', evt.data);
-		};
-		
-		this.conn.onclose = function(){dispatch('close',null)}
-		this.conn.onopen = function(){dispatch('open',null)}
-	};
-	
-	this.disconnect = function()
-	{
-		this.conn.close();
-	};
-	
-	var dispatch = function(event_name, message)
-	{
-		if(message == null || message == "")//aqui es donde se realiza toda la accion
-			{
-			}
-			else
-			{
-				pocision(message);
-			}
-	}
+    var callbacks = {};
+    var ws_url = url;
+    var conn;
+
+    this.bind = function(event_name, callback){
+        callbacks[event_name] = callbacks[event_name] || [];
+        callbacks[event_name].push(callback);
+        return this;// chainable
+    };
+
+    this.send = function(event_name, event_data){
+        this.conn.send( event_data );
+        return this;
+    };
+
+    this.connect = function() {
+        if ( typeof(MozWebSocket) == 'function' )
+            this.conn = new MozWebSocket(url);
+        else
+            this.conn = new WebSocket(url);
+
+        // dispatch to the right handlers
+        this.conn.onmessage = function(evt){
+            dispatch('message', evt.data);
+        };
+
+        this.conn.onclose = function(){dispatch('close',null)}
+        this.conn.onopen = function(){dispatch('open',null)}
+    };
+
+    this.disconnect = function() {
+        this.conn.close();
+    };
+
+    var dispatch = function(event_name, message){
+        var chain = callbacks[event_name];
+        if(typeof chain == 'undefined') return; // no callbacks for this event
+        for(var i = 0; i < chain.length; i++){
+            chain[i]( message )
+        }
+    }
 };
-
-var Server;
-function send( text ) 
-{
-    Server.send( 'message', text );
-}
-$(document).ready(function() 
-{
-	Server = new FancyWebSocket('https://univomaps.herokuapp.com/');
-    Server.bind('open', function()
-	{
-    });
-    Server.bind('close', function( data ) 
-	{
-    });
-    Server.bind('message', function( payload ) 
-	{
-    });
-    Server.connect();
-});
-
 
